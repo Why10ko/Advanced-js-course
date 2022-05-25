344f02a489796adfd56308d68daf3b8a79abfb85
Yurii Voytenko <why10ko@yandex.ru>
2022-05-25 20:40:45 +0300

    Default arguments. Function reduction. Removing commas

--- project/script.js
+++ project/script.js
@@ -1,3 +1,5 @@
+let classList = document.querySelector('.goods-list');
+
 const goods = [
   { title: 'Shirt', price: 150 },
   { title: 'Socks', price: 50 },
@@ -5,7 +7,7 @@ const goods = [
   { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title, price) => {
+const renderGoodsItem = (title = 'no product', price = 'no price') => {
   return `
     <div class="goods-item">
       <h3>${title}</h3>
@@ -15,8 +17,7 @@ const renderGoodsItem = (title, price) => {
 };
 
 const renderGoodsList = (list) => {
-  let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-  document.querySelector('.goods-list').innerHTML = goodsList;
+  classList.innerHTML = list.map(item => renderGoodsItem(item.title, item.price)).join('');
 }
 
 renderGoodsList(goods);
