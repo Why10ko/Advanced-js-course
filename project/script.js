f0d7b3cc660671962116ad7139a47808a2502859
Yurii Voytenko <why10ko@yandex.ru>
2022-06-28 10:13:31 +0300

    Move the search to a separate component

--- project/script.js
+++ project/script.js
@@ -8,13 +8,60 @@ function service(url) {
 }
 
 function init() {
+  const Search = Vue.component('search', {
+    props: [
+      'value'
+    ],
+    template: `
+    <div class="search">
+      <input type="text" class="goods-search" :value="value" @input="$emit('input', $event.target.value)" />
+     
+    </div>
+    `
+  })
+  const customButton = Vue.component('custom-button', {
+    template: `
+    <button class="search-button" type="button" v-on:click="$emit('click')">
+      <slot></slot>
+    </button>
+    `
+  })
+  const basketGoods = Vue.component('basket-goods', {
+    props: [
+      "isvisiblecart"
+    ],
+    template: `
+      <div class="fixed-area" v-if="isvisiblecart" >
+        <div class="basket-card">
+          <div class="basket-card__header">
+            <h1 class="basket-card__header__title">basket card</h1>
+            <div class="basket-card__header__delete-icon" v-on:click="$emit('closecart')"></div>
+          </div>
+          <div class="basket-card__content">
+            content
+          </div>
+        </div>
+      </div>
+      `
+  })
+  const goodsItem = Vue.component("goods-item", {
+    props: [
+      'item'
+    ],
+    template: `
+      <div class="goods-item">
+        <h3>{{ item.product_name }}</h3>
+        <p>{{ item.price }}</p>
+      </div>
+      `
+  })
+
   const app = new Vue({
     el: '#root',
     data: {
       items: [],
-      filteredItems: [],
       search: '',
-      isVisibleCart: true
+      isVisibleCart: false
     },
     methods: {
       fetchGoods() {
@@ -23,10 +70,8 @@ function init() {
           this.filteredItems = data;
         });
       },
-      filterItems() {
-        this.filteredItems = this.items.filter(({ product_name }) => {
-          return product_name.match(new RegExp(this.search, 'gui'))
-        })
+      searchChangehandler(value) {
+        this.search = value;
       },
       setVisionCart() {
         this.isVisibleCart = !this.isVisibleCart;
@@ -37,6 +82,11 @@ function init() {
         return this.filteredItems.reduce((prev, { price }) => {
           return prev + price;
         }, 0)
+      },
+      filteredItems() {
+        return this.filteredItems = this.items.filter(({ product_name }) => {
+          return product_name.match(new RegExp(this.search, 'gui'))
+        })
       }
     },
     mounted() {
